c68e3dee0c3e9aa710e95e0ccd11794ce0a3ce8a
tazro inutano ohta <inutano@gmail.com>
2015-03-12 15:55:26 +0900

    fixed to use gridengine

--- tool/fastq_dump.rb
+++ tool/fastq_dump.rb
@@ -4,67 +4,55 @@ require "rake"
 require "fileutils"
 
 Basedir = "/home/inutano/project/ER"
+GEadmin = "/home/geadmin/UGER/bin/lx-amd64"
 
-def next_items(num)
+def data_sort_by_size
+  ## Create a list of path to data sorted by data size
   data_dir = Basedir + "/data"
   sra_files = Dir.glob(data_dir + "/*sra")
-  sra_files.sort_by{|f| File.size(f) }.shift(num)
-end
-
-def fastq_dump(sra_file)
-  cmd = ""
-  cmd << "cd #{Basedir}/data && "
-  cmd << "/home/inutano/local/bin/sratoolkit/fastq-dump --split-3 "
-  cmd << sra_file
-  sh cmd
-  FileUtils.mv(Dir.glob(sra_file.gsub(/\.sra$/,"*.fastq")), Basedir + "/fastq")
-  FileUtils.rm_f(sra_file)
-  puts sra_file.split("/").last + " finished at " + Time.now.to_s
-rescue RuntimeError
-  FileUtils.mv(sra_file, Basedir + "/fqdumpfailed")
-  puts sra_file.split("/").last + " ------FAILED------ " + Time.now.to_s
+  sra_files.sort_by{|f| File.size(f) }
 end
 
 def disk_full?
   data_usage = `du /home/inutano/project/ER/data | cut -f 1`.chomp.to_i
   fastq_usage = `du /home/inutano/project/ER/fastq | cut -f 1`.chomp.to_i
   disk_usage = data_usage + fastq_usage
-  if fastq_usage > 20_000_000_000 or disk_usage > 40_000_000_000
-    true
+  fastq_usage > 20_000_000_000 or disk_usage > 40_000_000_000
+end
+
+def check_volume
+  ## wait if disk full
+  if disk_full?
+    puts "DISK FULL: WAIT, SINCE " + Time.now.to_s
+    while disk_full?
+      sleep 10
+    end
   end
+  true
+end
+
+def submit_fqdump(fpath, queue)
+  job_name = fpath.split("/").last.slice(0..8) + "D"
+  script_path = Basedir + "/tool/fastq_dump_single.sh"
+  sh "/home/geadmin/UGER/bin/lx-amd64/qsub -N #{job_name} -l #{queue} #{script_path} #{fpath}"
+rescue RunTimeError
+  qstat = "#{GEadmin}/qstat | grep 'inutano' | wc -l".to_i
+  if qstat > 4500
+    puts "---- too many job! ----"
+    while qstat > 4500
+      sleep 300
+    end
+    retry
+  end
+  puts "------ qsub command caused an error for #{fpath} " + Time.now.to_s
+  exit 1
 end
 
 if __FILE__ == $0
+  Queue = ARGV.first || "short"
   while true
-    # anytime disk full
-    if disk_full?
-      puts "Disk quota nearly exceeded: sleep until anyone is out " + Time.now.to_s
-      while disk_full?
-        sleep 10
-      end
+    data_sort_by_size.each do |fpath|
+      submit_fqdump(fpath, Queue) if chack_volume
     end
-
-    # fix the number to change throughput
-    number_of_parallel = 14
-    srafiles = next_items(number_of_parallel).compact
-
-    # no file to dump
-    if srafiles.empty?
-      puts "No file to dump: sleep until new guys are coming " + Time.now.to_s
-      while srafiles.empty?
-        sleep 10
-        srafiles = next_items(number_of_parallel).compact
-      end
-    end
-    
-    # start processing
-    threads = []
-    srafiles.each do |srafile|
-      th = Thread.new do
-        fastq_dump(srafile)
-      end
-      threads << th
-    end
-    threads.each{|th| th.join }
   end
 end
