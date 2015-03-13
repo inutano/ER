cbec66dc53530a11177d56bc88d5562e9bbaddfc
tazro inutano ohta <inutano@gmail.com>
2015-03-13 11:09:08 +0900

    modified file move

--- tool/fastq_dump_single.sh
+++ tool/fastq_dump_single.sh
@@ -1,7 +1,8 @@
 #$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=4G,s_vmem=4G -pe def_slot 1
 # ls data/*.sra | sed -e 's:data/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
 
-cd /home/inutano/project/ER/data
+BASEDIR="/home/inutano/project/ER"
+cd "${BASEDIR}/data"
 /home/inutano/local/bin/sratoolkit/fastq-dump --split-3 $1 && rm -f $1
 id=`echo $1 | sed -e 's:\.sra$::' | sed -e 's:\.lite$::'`
-ls /home/inutano/project/ER/data | awk '$0 ~ /'"${id}"'/' | xargs -0 mv -t /home/inutano/project/ER/fastq
+mv "${BASEDIR}/data/${id}*.fastq" "${BASEDIR}/fastq"
