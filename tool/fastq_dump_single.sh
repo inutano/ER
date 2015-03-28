585269fec8d70f0e263b21f6013c56e10276c21c
tazro inutano ohta <inutano@gmail.com>
2015-03-28 15:09:41 +0900

    fixed to use /ssd

--- tool/fastq_dump_single.sh
+++ tool/fastq_dump_single.sh
@@ -1,8 +1,25 @@
 #$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=4G,s_vmem=4G -pe def_slot 1
 # ls data/*.sra | sed -e 's:data/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
 
+# Get a path to dump command
+fqdump="/home/inutano/local/bin/sratoolkit/fastq-dump --split-3"
+
+# Acquiring Location..
 BASEDIR="/home/inutano/project/ER"
-cd "${BASEDIR}/data"
-/home/inutano/local/bin/sratoolkit/fastq-dump --split-3 $1 && rm -f $1
+fqdir="${BASEDIR}/fastq"
+
+# extract id from file name since argument 1 is a path to sra file
 id=`echo $1 | sed -e 's:\.sra$::' | sed -e 's:\.lite$::'`
-mv ${id}*.fastq ${BASEDIR}/fastq
+
+if [ -e "/ssd" ] ; then
+  stage=/ssd/$id
+  mkdir -p $stage
+  mv $1 $stage
+  cd $stage && ls *sra | xargs $fqdump
+  mv ${stage}/*fastq $fqdir && rm -fr $stage || rm -fr $stage && echo $1 >> ${BASEDIR}/table/fqdumpfailed
+else
+  cd "${BASEDIR}/data"
+  $fqdump $1 && rm -f $1
+  id=`echo $1 | sed -e 's:\.sra$::' | sed -e 's:\.lite$::'`
+  mv ${id}*.fastq $fqdir
+fi
