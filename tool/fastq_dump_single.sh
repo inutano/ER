f876f5998fd6dadd684806018df8af0de8354d19
tazro inutano ohta <inutano@gmail.com>
2015-04-03 17:02:19 +0900

    refactored

--- tool/fastq_dump_single.sh
+++ tool/fastq_dump_single.sh
@@ -1,25 +1,39 @@
 #$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=4G,s_vmem=4G -pe def_slot 1
-# ls data/*.sra | sed -e 's:data/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
+# Usage:
+#   fastq_dump.single.sh /path/to/DRR000001.sra
+# Interactive Run:
+#   ls data/*.sra | sed -e 's:data/::g' | while read f ; do ; qsub -N $f fastq_dump_single.sh $f ; done
 
-# Get a path to dump command
+set -u
+
+## User Setting
+# path to dump command
 fqdump="/home/inutano/local/bin/sratoolkit/fastq-dump --gzip --split-3"
 
-# Acquiring Location..
-BASEDIR="/home/inutano/project/ER"
-fqdir="${BASEDIR}/fastq"
+# working directory
+DIR_IN="/home/inutano/project/ER/fastq"
+DIR_OUT="/home/inutano/project/ER/data"
+FQLOG="/home/inutano/project/ER/table/fqdumpfailed"
 
-# extract id from file name since argument 1 is a path to sra file
-id=`echo $1 | sed -e 's:\.sra$::' | sed -e 's:\.lite$::'`
+## Run fastq-dump
+# File exist?
+filepath=${1}
+if [ ! -e ${filepath} ] ; then
+  exit 1
+fi
 
+# Extract filename and ID from file path
+filename=`echo ${filepath} | tr '/' '\n' | awk '$0 ~ /^.RR/'`
+id=`echo ${filename} | awk -F '.' '{ print $1 }'`
+
+# dump on /ssd if available
 if [ -e "/ssd" ] ; then
-  stage=/ssd/$id
-  mkdir -p $stage
-  mv $1 $stage
-  cd $stage && ls *sra | xargs $fqdump
-  mv ${stage}/*fastq.gz $fqdir && rm -fr $stage || rm -fr $stage && echo $1 >> ${BASEDIR}/table/fqdumpfailed
+  stage=/ssd/${id}
+  mkdir -p ${stage}
+  mv ${filepath} ${stage}
+  cd ${stage}
+  ${fqdump} ${filename} && mv ${stage}/*fastq* && rm -fr ${stage} || rm -fr ${stage} && echo ${filepath} >> ${FQLOG}
 else
-  cd "${BASEDIR}/data"
-  $fqdump $1 && rm -f $1
-  id=`echo $1 | sed -e 's:\.sra$::' | sed -e 's:\.lite$::'`
-  mv ${id}*.fastq.gz $fqdir
+  cd ${DIR_IN}
+  ${fqdump} ${filepath} && mv ${id}*fastq* ${DIR_OUT} && rm -f ${filepath}
 fi
