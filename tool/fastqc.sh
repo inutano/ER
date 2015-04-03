450cd896184856eac08840ae104a1b546fce86a2
tazro inutano ohta <inutano@gmail.com>
2015-04-03 17:52:10 +0900

    refactored

--- tool/fastqc.sh
+++ tool/fastqc.sh
@@ -1,5 +1,36 @@
 #$ -o /home/inutano/project/ER/log -S /bin/bash -j y -l mem_req=2G,s_vmem=2G -pe def_slot 4
-# ls fastq/ | sed -e 's:fastq/::g' | while read f ; do ; qsub -N $f simple_fastqc.sh $f ; done
+# Usage:
+#   fastqc.sh /path/to/DRR000001.fastq.gz
+set -u
 
-cd /home/inutano/project/ER/fastq
-/home/inutano/local/bin/fastqc --thread 4 --noextract $1 && rm -f $1
+## User Setting
+# path to fastqc command
+fastqc="/home/inutano/local/bin/fastqc --thread 4 --noextract"
+
+# working directory
+DIR_IN="/home/inutano/project/ER/fastq"
+DIR_OUT="/home/inutano/project/ER/fastq"
+FQLOG="/home/inutano/project/ER/table/fqdumpfailed"
+
+## Run fastqc
+# File exist?
+filepath=${1}
+if [ ! -e ${filepath} ] ; then
+  exit 1
+fi
+
+# Extract filename and ID from file path
+filename=`echo ${filepath} | tr '/' '\n' | awk '$0 ~ /^.RR/'`
+id=`echo ${filename} | awk -F '.' '{ print $1 }'`
+
+# run on /ssd if available
+if [ -e "/ssd" ] ; then
+  stage=/ssd/fastqc/${id}
+  mkdir -p ${stage}
+  mv ${filepath} ${stage}
+  cd ${stage}
+  ${fastqc} ${filename} && mv ${stage}/*.zip ${DIR_OUT} && rm -fr ${stage} || rm -fr ${stage} && echo ${filepath} >> ${FQLOG}
+else
+  cd ${DIR_IN}
+  ${fastqc} ${filepath} && mv ${id}*zip && rm -f ${filepath}
+fi
