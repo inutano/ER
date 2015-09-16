74e5bf67dd2a94815ff3257940c06d0d8b22bb62
tazro inutano ohta <inutano@gmail.com>
2015-09-16 21:27:40 +0900

    be kind to filesystem

--- tool/file_transfer.rb
+++ tool/file_transfer.rb
@@ -3,6 +3,7 @@
 require "fileutils"
 
 BASE = "/home/inutano"
+PROJ_BASE = File.join(BASE, "project/ER")
 
 def disk_full?
   data_usage = `du /home/inutano/project/ER/data 2> /dev/null | cut -f 1`.chomp.to_i
@@ -12,16 +13,26 @@ def disk_full?
 end
 
 def wd
-  { download_dir: BASE + "/project/ER/download",
-    download_log: BASE + "/project/ER/table/download_log",
-    download_notfound: BASE + "/project/ER/table/download_notfound",
-    data_dir: BASE + "/project/ER/data" }
+  { download_dir:      PROJ_BASE + "/download",
+    download_log:      PROJ_BASE + "/table/download_log",
+    download_notfound: PROJ_BASE + "/table/download_notfound",
+    data_dir:          PROJ_BASE + "/data",
+    fastq_dir:         PROJ_BASE + "/fastq" }
 end
 
 def file_transfer(fpath)
   fname = fpath.split("/").last
   FileUtils.cp(fpath, wd[:download_dir])
-  FileUtils.mv(File.join(wd[:download_dir], fname), wd[:data_dir])
+  target_dir = case fname
+               when /sra$/
+                 File.join(wd[:data_dir], fname.gsub(/...\.sra$/,""))
+               when /bz2$/
+                 wd[:fastq_dir]
+               else
+                 wd[:data_dir]
+               end
+  FileUtils.mkdir(target_dir) if !File.exist?(target_dir)
+  FileUtils.mv(File.join(wd[:download_dir], fname), target_dir)
   open(wd[:download_log],"a"){|f| f.puts(fname) }
 rescue Errno::ENOENT
   open(wd[:download_notfound],"a"){|f| f.puts(fpath+"\tnot found") }
@@ -30,7 +41,7 @@ rescue Errno::EISDIR
 end
 
 if __FILE__ == $0
-  filelist_path = ARGV.first || BASE + "/project/ER/table/filelist"
+  filelist_path = ARGV.first || PROJ_BASE + "/table/filelist"
   filelist = open(filelist_path).readlines
   
   progress = 0
